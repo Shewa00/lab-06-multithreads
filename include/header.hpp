003edcb4798cea54b268552063e66dfafc3c9e55
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2020-01-13 10:20:45 +0300

    Update header.hpp

--- include/header.hpp
+++ include/header.hpp
@@ -1,6 +1,34 @@
 // Copyright 2018 Your Name <your_email>
+#pragma once
 
-#ifndef INCLUDE_HEADER_HPP_
-#define INCLUDE_HEADER_HPP_
+#include <picosha2.h>
+#include <boost/log/trivial.hpp>
+#include <string>
+#include <thread>
 
-#endif // INCLUDE_HEADER_HPP_
+static const std::string endOfHash = "0000";
+
+class Hash
+{
+public:
+    static void hashMake() {
+        while (true)
+        {
+            std::string randStringHash = std::to_string(std::rand());
+            std::string hashOfRandString = picosha2::hash256_hex_string(randStringHash);
+
+            if (hashOfRandString.substr(hashOfRandString.size() - endOfHash.size()) == endOfHash) {
+
+                BOOST_LOG_TRIVIAL(info) << "Found correct value '" << randStringHash
+                    << "' with hash '" << hashOfRandString
+                    << "'";
+            }
+            else {
+                BOOST_LOG_TRIVIAL(trace) << "Thread with ID: " << std::this_thread::get_id()
+                    << " uncorrect value '" << randStringHash
+                    << "' with hash '" << hashOfRandString
+                    << "'";
+            }
+        }
+    }
+};
