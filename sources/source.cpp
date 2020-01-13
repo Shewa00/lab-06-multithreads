de8b33ea80f6324cb38bc8289c5757eec5c28e78
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2020-01-13 10:24:01 +0300

    Update source.cpp

--- sources/source.cpp
+++ sources/source.cpp
@@ -1,3 +1,35 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2020 Ara Ovsepyan ara07.99@icloud.com
 
-#include <header.hpp>
+#include <hashMake.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/log/utility/setup.hpp>
+#include <thread>
+#include <vector>
+
+
+int main(int argc, char* argv[])
+{
+    srand(time(nullptr));                // иниц рандом
+    boost::log::add_file_log("Result.log"); // создаем файл, куда будем выгружаем
+    boost::log::add_console_log(std::cout);
+    size_t countOfThreads = 0; // количество потоков
+    if (argc < 2) {
+        countOfThreads = std::thread::hardware_concurrency(); // макс количество потоков, которые можно обрабатывать
+    }
+    else {
+        countOfThreads = boost::lexical_cast<size_t>(argv[1]); // принимаем 1е значение
+    }
+
+    std::vector<std::thread> threads;
+    threads.reserve(countOfThreads);
+
+    for (size_t i = 0; i < countOfThreads; ++i) {
+        threads.emplace_back(Hash::hashMake);
+    }
+
+    for (std::thread& i : threads) {
+        i.join();
+    }
+
+    return 0;
+}
