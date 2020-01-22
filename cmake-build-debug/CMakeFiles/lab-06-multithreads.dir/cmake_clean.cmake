6a3c021444e6f99f98d901a7220071faa8e05746
Shewa00 <KiraKryt63@mail.ru>
2020-01-22 11:03:40 +0300

    trqwert

---
+++ cmake-build-debug/CMakeFiles/lab-06-multithreads.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab-06-multithreads.dir/sources/source.cpp.o"
+  "lab-06-multithreads"
+  "lab-06-multithreads.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab-06-multithreads.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
